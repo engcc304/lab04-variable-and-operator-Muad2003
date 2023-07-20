649f435bc7c53aaac9f38d1aa66754051f5ba666
Muad2003 <anuphap2003118@gmail.com>
2023-07-20 15:44:36 +0700

    update lap4

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -26,10 +26,17 @@
         Salary = U$ 374000.00
 */
 #include <stdio.h>
-
-int main() {
-
-    //--| YOUR CODE HERE
-
+int main(){
+    int Employees_Id[10] ;
+    int  working_hr ;
+    int Salary_amount_hr ;
+    printf( "Enter Your Id: " ) ; 
+    scanf( "%d", Employees_Id ) ;
+    printf( "Enter Your Hr: ") ;
+    scanf( "%d", working_hr) ;
+    printf( " Enter Your Salary" ) ;
+    scanf( "%d", Salary_amount_hr ) ;
+    printf( "You Employees Id =", Employees_Id ) ;
+    printf ( "Your Salary is",working_hr * Salary_amount_hr ) ;
     return 0 ;
 }//end main function
