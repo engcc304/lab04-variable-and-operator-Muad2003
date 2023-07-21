1097ef12db31742116be294a682e63e84368a51e
Muad2003 <anuphap2003118@gmail.com>
2023-07-21 14:38:04 +0700

    update lap4

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -42,7 +42,7 @@ int main(){
 
     Salary = Salary_amount_hr * working_hr ;
     printf("Expected Output:\n");
-    printf("Employees ID = %s\n",EmployeesID) ;
+    printf("EmployeesID= %s\n",EmployeesID) ;
     printf("Salary = U$ %f\n",Salary) ;
 
     return 0 ;
